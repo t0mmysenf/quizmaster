bace27058b3921a562d9119b5b29a25e4a1036ff
Peter Gisler <peter.gisler@gibz.ch>
2019-11-18 21:05:27 +0100

    Update install script

--- test.sh
+++ test.sh
@@ -29,16 +29,6 @@ dbConfiguration=(
 # Change to quiz directory
 cd /var/www/html/m150/quiz
 
-# Use composer to install dependencies
-sudo composer install
-
-# Build frontend using npm
-sudo npm install
-
-# Setup the database
-mysql -user=root -password=${dbRootPassword} < /var/www/html/m150/initDb.sql
-mysql -user=quizmaster -password=${dbPassword} quiz < /var/www/html/m150/seedDb.sql
-
 # Prepare the laravel framework
 sudo cp .env.example .env
 
@@ -48,17 +38,3 @@ do
     replace=${dbConfiguration[$i]}
     sed -i "" "s/${search}/${replace}/g" .env
 done
-
-# Generate application keys
-sudo php artisan key:generate
-sudo php artisan passport:keys
-
-# Fix file permissions
-# Assign 'apache' as user and user group recursively to the web root directory
-sudo chown -R apache:apache /var/www/html
-# Adjust permissions for some directories (recursively)
-sudo chmod -R 0775 /var/www/html/m150/quiz/storage/ /var/www/html/m150/quiz/bootstrap/cache/
-
-# Restart apache webserver
-sudo systemctl restart httpd
-
